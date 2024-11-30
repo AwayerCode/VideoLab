bcbbbcbb9281bcd821c78729e5fa441a48098584
guoxudong <510105194@qq.com>
2024-12-01 00:00:21 +0800

    Remove unused source files and clean up CMakeLists.txt
    - Deleted GlobalRoute.cpp, GlobalRoute.h, FileInfo.cpp, FileInfo.h, and MemMonitor files to streamline the project.
    - Updated CMakeLists.txt to reflect the removal of these files and clean up the source file list.

--- src/FileInfo/FileInfo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-class FileInfo {
-public:
-    FileInfo();
-    ~FileInfo();
-};  
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
 #include <string>
-#include "FileInfo/FileInfo.h"
-#include "GlobalRoute.h"
+
 
 int main() {
-    std::string input;
     
-    while (true) {
-        std::cout << "Enter command (type 'quit' to exit): ";
-        std::getline(std::cin, input);
-        
-        if (input == "quit") {
-            break;
-        }
-
-        GlobalRoute::getInstance().processCommand(input);
-        
-        std::cout << "You entered: " << input << std::endl;
-    }
-    
-    std::cout << "Program terminated." << std::endl;
     return 0;
 }   
